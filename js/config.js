e2f64dd9a756faf2e16f295f5b13199d644fcfef
Rakshit Pandey <rakshitpandey0321@gmail.com>
2026-07-19 20:13:58 +0530

    Point frontend to deployed Render backend

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Change this one line once your backend is deployed (e.g. Render).
 // Everything else in the site reads from here.
-const API_URL = "http://localhost:3000";
+const API_URL = "https://taste-haven-restaurant-3yuj.onrender.com";
